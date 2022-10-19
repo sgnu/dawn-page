6826a2f2fb766b34487d5791e578a5990ff5c24c
sgnu <sam.nguyen.1223@gmail.com>
2022-10-19 15:45:09 -0400

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+// import HelloWorld from './components/HelloWorld.vue'
+// import TheWelcome from './components/TheWelcome.vue'
+</script>
+
+<template>
+  <h1>Hello World!</h1>
+</template>
+
+<style scoped>
+</style>
