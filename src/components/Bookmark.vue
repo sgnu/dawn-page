b5d62f166e773d32e700f038752a7370142c575a
sgnu <sam.nguyen.1223@gmail.com>
2022-10-20 07:09:43 -0400

    Change text to centered

--- src/components/Bookmark.vue
+++ src/components/Bookmark.vue
@@ -5,8 +5,9 @@ export default {
         bookmarks: Array
     },
     methods: {
-        openWindow(url) {
-            window.open(url)
+        openWindow(bookmark) {
+            window.alert(`This will open ${bookmark.name} at ${bookmark.url}`)
+            // window.open(bookmark.url, '_self') // opens in current tab and not a new one
         }
     }
 }
@@ -14,7 +15,7 @@ export default {
 
 <template>
     <div class="bookmarks-container">
-        <div class="bookmark" v-for="bookmark in bookmarks" v-on:click="openWindow(bookmark.url)">
+        <div class="bookmark" v-for="bookmark in bookmarks" v-on:click="openWindow(bookmark)">
             <h1 class="short-form" :style="{ 'color': bookmark.color }">{{ bookmark.shortForm }}</h1>
             <p class="name">{{ bookmark.name }}</p>
             <p class="url">{{ bookmark.url }}</p>
@@ -26,5 +27,9 @@ export default {
 .bookmarks-container {
     display: flex;
     flex-direction: column;
+}
+
+.bookmark {
+    text-align: center;
 }
 </style>
